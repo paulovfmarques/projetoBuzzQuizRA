0944cc15c9cff9c59db0d4f109c5395e14783fcb
Paulo Vitor <marquespaulov@gmail.com>
2020-09-22 14:54:30 -0300

    Quiz hub created

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,9 @@
-let dados = {
-    email : "",
-    password : ""
-}
+let dados = {email : "", password : ""}
+let token = "";
+const urlPostUsuario = "https://mock-api.bootcamp.respondeai.com.br/api/v1/buzzquizz/users";
+const urlPostQuiz = "https://mock-api.bootcamp.respondeai.com.br/api/v1/buzzquizz/quizzes";
+const urlGetQuiz = "https://mock-api.bootcamp.respondeai.com.br/api/v1/buzzquizz/quizzes";
 
-var token;
 
 function conferirCamposLogin(){
 
@@ -23,28 +23,36 @@ function conferirCamposLogin(){
     cadastro();    
 }
 
+function processarEnter(e){
+    if (e.keyCode == 13) {
+        conferirCamposLogin();
+        return false;
+    }
+}
+
 function cadastro(){
 
     var btnEntrar = document.querySelector(".entrar");
     btnEntrar.disabled = true;
 
-    var requisição = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v1/buzzquizz/users", dados)
+    var requisição = axios.post(urlPostUsuario, dados)
     
-    requisição.then(processaCadastro).catch(trataErro)
+    requisição.then(processaCadastro).catch(trataErroCadastro)
 }
 
-function processaCadastro(data){
+function processaCadastro(resposta){
 
     var btnEntrar = document.querySelector(".entrar");
     btnEntrar.disabled = false;
 
-    token = data.token;
+    token = resposta.data.token;
+    console.log(token)
 
     mudandoParaQuizzes();
-
+    setTimeout(carregandoQuizzes, 1000);
 }
 
-function trataErro(){
+function trataErroCadastro(){
     alert("Usuário/Senha Incorretos!");
     window.location.reload()
 }
@@ -52,5 +60,28 @@ function trataErro(){
 function mudandoParaQuizzes(){
     
     var telaInicial = document.querySelector("body > header");
-    telaInicial.style.display = "none";
+    var telaQuizzes = document.querySelector(".menu-quizzes");
+
+    telaInicial.classList.add("fade-out");
+    //telaInicial.style.display = "none";
+
+    telaQuizzes.classList.add("fade-in");
+    telaQuizzes.style.display = "block";
+}
+
+function carregandoQuizzes(){    
+    var quizzes = axios.get(urlGetQuiz, {headers : {"User-token" : token}});
+    quizzes.then(processaQuizzes).catch(trataErroMenu)
+}
+
+function processaQuizzes(info){
+    console.log(info)
+    // fazer função que imprime na tela os quizzes já criados
+    
+    //fazer função que muda para a tela de criação de quiz
+}
+
+function trataErroMenu(){
+    alert("Fatal Error: Falha no carregamento dos quizzes!");
+    window.location.reload()
 }
